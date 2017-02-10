cc9ef9e2c70ad9ca6a343b1e230bf2a2de3935c8
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-02-10 12:56:14 -0800

    add noMatch component

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class NoMatch extends Component {
   render() {
     return (
-      <div>
-        404 - Invalid url
+      <div className="invalid-url">
+        <div className="invalid-url-content">
+          404 - Invalid url
+        </div>
       </div>
     );
   }
